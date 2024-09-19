8144f6084980ec0290adf06cdeed48a1b8818159
Bharath-Hebbar <bharath.hebbar@leadpulse.io>
2024-09-19 20:55:21 +0530

    change id

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default async function Home() {
   const experience = await fetchBySlug({
     client,
     slug: 'home',
-    experienceTypeId: 'landingPage',
+    experienceTypeId: 'leadpulseLandingPage',
     localeCode: 'en-US'
   })
 
