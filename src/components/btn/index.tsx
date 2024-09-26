89b61d184b69149369b4c00fb0a51a9e58c9e8d8
Bharath-Hebbar <bharath.hebbar@leadpulse.io>
2024-09-26 19:05:10 +0530

    changes

--- src/components/btn/index.tsx
+++ src/components/btn/index.tsx
@@ -1,19 +1,67 @@
+'use client'
+
 import { ComponentDefinition } from '@contentful/experiences-sdk-react'
+import { cva, cx, type VariantProps } from 'class-variance-authority'
 
-interface Props {
-  text: string
+import { Chip } from '@nextui-org/react'
+import Link from 'next/link'
+
+export interface Props extends React.HTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVars> {
+  label: string
+  listText: any[]
+  sideEffect?: 'alertMe' | 'consoleLog'
+  className?: string
+  url?: string
+  random: any
 }
 
-export function Button({ text }: Props) {
-  return <button>{text}</button>
+export function Button({ label, className, sideEffect, random, url, listText, variant = 'primary', ...ctflProps }: Props) {
+  const handleClick = () => {
+    if (sideEffect === 'alertMe') {
+      alert('You clicked on me')
+    } else {
+      console.log('You clicked on me but ama log this')
+    }
+  }
+
+  if (url) {
+    return (
+      <Link href={url}>
+        <div className={cx(className, buttonVars({ variant }))}>{label}</div>
+      </Link>
+    )
+  }
+
+  return (
+    <button onClick={handleClick} className={cx(className, buttonVars({ variant }))} {...ctflProps}>
+      hello
+      {listText &&
+        listText?.map((txt, txidx) => {
+          return <Chip key={txidx}>Chip</Chip>
+        })}
+      {label}
+    </button>
+  )
 }
+
+const buttonVars = cva('base-btn ', {
+  variants: {
+    variant: {
+      primary: 'btn-primary',
+      secondary: 'btn-secondary'
+    }
+  },
+  defaultVariants: {
+    variant: 'primary'
+  }
+})
 
 const thumbnailUrl = 'https://images.ctfassets.net/drya7kvck7n6/7wrYr53XFL9jeOOwLvh0Sq/49d80dc701b46848be1a57157a460436/image.png'
 
 export const buttonDefinition: ComponentDefinition = {
-  id: 'test',
-  name: 'New Comp',
-  category: 'Custom Components',
+  id: 'button',
+  name: 'Button',
+  category: 'Base',
   thumbnailUrl: thumbnailUrl,
   builtInStyles: ['cfBackgroundColor'],
   tooltip: {
@@ -48,6 +96,7 @@ export const buttonDefinition: ComponentDefinition = {
       defaultValue: '/',
       hyperlinkPattern: '/{locale}/experience/{entry.fields.slug}'
     },
+
     random: {
       displayName: 'Random Array Example',
       type: 'Array',
